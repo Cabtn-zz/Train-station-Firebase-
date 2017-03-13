6b8a128a31c681155277c040ddb98d07ebbc95b0
Abtin Ravanipour <Abtin Ravanipour>
2017-03-13 00:40:23 -0700

    still need to do firebase

---
+++ train.js
@@ -0,0 +1,51 @@
+  var config = {
+    apiKey: "AIzaSyBg6DPAQ5PxJ-8zZu5gRnPPjGvd7XsJ0Ss",
+    authDomain: "train-station-935b6.firebaseapp.com",
+    databaseURL: "https://train-station-935b6.firebaseio.com",
+    storageBucket: "train-station-935b6.appspot.com",
+    messagingSenderId: "943735859652"
+  };
+  firebase.initializeApp(config);
+
+  var trainArray = []
+  var database = firebase.database();
+  var listCount = 0;
+
+ //  database.ref().on("value", function(snapshot){
+	// if (snapshot.val().trains) {
+	// 	trainArray = snapshot.val().trains;
+	// 	var allTrains = snapshot.val().trains;
+	// }  	
+	// else {
+	// 	trainArray.push(newTrain);
+	// }
+ //  })
+
+  $("#submit").on("click", function(){
+  	event.preventDefault();
+  	console.log("button works")
+  	var newTrain = {
+  		name: $("#trainInput").val(),
+  		destination: $("#destinationInput").val(),
+  		frequency: $("#frequencyInput").val(),
+  		time: $("#timeInput").val(),
+  		minutesAway: $("#minutesInput").val(),
+  	}
+  	console.log(newTrain);
+  	trainArray.push(newTrain);
+  	console.log(trainArray);
+  	addToList();
+  });
+
+  function addToList() {
+  	for (var i = 0; i < trainArray.length; i++){
+  		$(newTable).empty();
+  		var newTable = $("<tr>");
+  		$(newTable).append("<td>" + trainArray[i].name + "</td>");
+  		$(newTable).append("<td>" + trainArray[i].destination + "</td>");
+  		$(newTable).append("<td>" + trainArray[i].frequency + "</td>");
+  		$(newTable).append("<td>" + trainArray[i].time + "</td>");
+  		$(newTable).append("<td>" + trainArray[i].minutesAway + "</td>");
+  		$(".appendHere").append(newTable);
+  	}
+  }
